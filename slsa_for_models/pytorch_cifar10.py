fd691415c67ba31f8ed231eccf78bed356ab2035
Mihai Maruseac <mihaimaruseac@google.com>
2024-04-15 10:09:44 -0700

    Fix PT?
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>

--- slsa_for_models/pytorch_cifar10.py
+++ slsa_for_models/pytorch_cifar10.py
@@ -77,6 +77,29 @@ def load_data():
     return trainloader, testloader
 
 
+# Train a model based on tutorial from
+# https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html.
+# We inline the class to be able to use lazy loading of PyTorch modules.
+class MyModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
 def create_model():
     """Create a Torch NN model.
 
@@ -85,27 +108,6 @@ def create_model():
 
     Returns the model.
     """
-    # Train a model based on tutorial from
-    # https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html.
-    # We inline the class to be able to use lazy loading of PyTorch modules.
-    class MyModel(nn.Module):
-        def __init__(self):
-            super().__init__()
-            self.conv1 = nn.Conv2d(3, 6, 5)
-            self.pool = nn.MaxPool2d(2, 2)
-            self.conv2 = nn.Conv2d(6, 16, 5)
-            self.fc1 = nn.Linear(16 * 5 * 5, 120)
-            self.fc2 = nn.Linear(120, 84)
-            self.fc3 = nn.Linear(84, 10)
-
-        def forward(self, x):
-            x = self.pool(F.relu(self.conv1(x)))
-            x = self.pool(F.relu(self.conv2(x)))
-            x = torch.flatten(x, 1)
-            x = F.relu(self.fc1(x))
-            x = F.relu(self.fc2(x))
-            x = self.fc3(x)
-            return x
 
     return MyModel()
 
